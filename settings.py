a5de212fe4253ad75d132d0291e783643de6b3a3
Epoc <contact@epoc.fr>
2017-07-30 19:29:37 +0200

    Update default settings

--- settings.py
+++ settings.py
@@ -22,7 +22,7 @@ RESOURCES_ROOT = os.path.join(sys._MEIPASS, 'resources') if getattr(sys, 'frozen
 
 CONFIG_FILE = 'connectfour.ini'
 DEFAULT_CONFIG = {
-    'master_server_endpoint': 'https://cfg.epoc.fr/api/',
+    'master_server_endpoint': 'https://cfms.epoc.fr/api/',
     'sounds_volume': 0.1,
     'music_volume': 0.2
 }
